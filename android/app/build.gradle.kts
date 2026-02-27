dba1340cf3d924834117dcf5c44d9849a174ebcb
dondiego2020 <263612477+dondiego2020@users.noreply.github.com>
2026-02-27 14:08:45 +0330

    chore(android): bump versionCode to 4, versionName to 1.2.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.phoenix.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
